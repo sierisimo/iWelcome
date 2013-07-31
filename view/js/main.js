7a38232c286f75738ea4c97f864827092499965e
Sier Violencia <sierisimo@gmail.com>
2013-07-31 12:59:47 -0500

    Fast-dirty fix to reversed letters

--- view/js/main.js
+++ view/js/main.js
@@ -5,28 +5,28 @@
 			"inteliYellow-bg","inteliPurple-bg"],
 		divs = ["#upper_left_","#upper_right_","#bottom_right_","#bottom_left_"],
 		clock = 0, sides = ["left","up","right","down"],
-		states = {
-			0:{
+		states = [
+			{
 				showing:"first",
 				color: colors[Math.floor(Math.random()*colors.length)],
 				enterBy: sides[0]
 			},
-			1:{
+			{
 				showing:"first",
 				color: colors[Math.floor(Math.random()*colors.length)],
 				enterBy: sides[1]
 			},
-			2:{
+			{
 				showing:"first",
 				color: colors[Math.floor(Math.random()*colors.length)],
 				enterBy: sides[2]
 			},
-			3:{
+			{
 				showing:"first",
 				color: colors[Math.floor(Math.random()*colors.length)],
 				enterBy: sides[3]
 			}
-		};
+		];
 
 	(function(){
 		for(var i = 0; i < divs.length ; i++){
@@ -42,21 +42,21 @@
 		    x = $(divs[clock]+old).width() + 'px',
 		    moveTo = states[clock].enterBy;
 		
+
 		$(divs[clock]+actual)
 			.removeClass('hidden')
 			.removeClass(oldClass)
 			.addClass(colors[newColor])
-			.transition({perspective:x,rotateX:'180deg'});	
-		
-		$(divs[clock]+old).removeClass(oldClass).addClass(colors[newColor])
-			.transition({perspective:x,rotateX:'180deg'});
+			.transition({rotateX:'360deg', duration:800});
+
 
 		$(divs[clock]+old).addClass('hidden');
 		$(divs[clock]+old).removeAttr('style');
 
 		states[clock].showing = actual;
 		states[clock].color = colors[newColor];
-	
+		
+		//$(divs[clock]+actual).attr('style','');
 		clock++;
 		if(clock > 3)
 			clock=0;
@@ -69,8 +69,8 @@
 		},3000);
 
 	}
-	setInterval(sayHi,8000);
-	var time = setInterval(changeColor,5000);
-	setTimeout(function(){clearInterval(time);},200000);
+	//setInterval(sayHi,8000);
+	var time = setInterval(changeColor,2000);
+	setTimeout(function(){clearInterval(time);},100000);
 
 })();
